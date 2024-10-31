be03175852a0a7e4ce935f926c1f956f62f51cb1
KateO-R <veg2710@gmail.com>
2024-10-31 11:39:38 +0500

    hometask code completed

--- zeroCoder/MyApp/views.py
+++ zeroCoder/MyApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1>This is the first page of MyApp!</h1>")
+def data(request):
+    return HttpResponse("<h1>This is the data page of MyApp!</h1>")
+
+def test(request):
+    return HttpResponse("<h1>This is the test page of MyApp!</h1>")
