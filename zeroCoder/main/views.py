c96de3c7897647310b51d208e19a01fdea421850
KateO-R <veg2710@gmail.com>
2024-10-31 11:21:14 +0500

    MyApp created

--- zeroCoder/main/views.py
+++ zeroCoder/main/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>This is my first Django Project!</h1>")
+def new(request):
+    return HttpResponse("<h1>This is second page of the Django Project!</h1>")
