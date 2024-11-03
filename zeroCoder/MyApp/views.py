260f1337962b50e730bcce6ac054d85195ecc68b
KateO-R <veg2710@gmail.com>
2024-11-03 16:36:11 +0500

    continue code writing

--- zeroCoder/MyApp/views.py
+++ zeroCoder/MyApp/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>This is the first page of MyApp!</h1>")
-def data(request):
-    return HttpResponse("<h1>This is the data page of MyApp!</h1>")
+    return render(request, 'MyApp/index.html')
+def news(request):
+    return render(request, 'MyApp/news.html')
 
 def test(request):
     return HttpResponse("<h1>This is the test page of MyApp!</h1>")
