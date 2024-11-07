360d2f7bc16875ddcdabeb27c3737f6a82d2fd3e
KateO-R <veg2710@gmail.com>
2024-11-07 16:41:46 +0500

    final code for the lesson added

--- zeroCoder/news1/models.py
+++ zeroCoder/news1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class News_post(models.Model):
+    title = models.CharField('News title', max_length=100)
+    short_description = models.CharField('Short news description', max_length=200)
+    text = models.TextField('News')
+    pub_date = models.DateTimeField('Publication date')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Author')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'News Post'
+        verbose_name_plural = 'News Posts'
+
--- zeroCoder/news1/urls.py
+++ zeroCoder/news1/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="news1.home")
+    path('', views.home, name="news1_home")
 ]
